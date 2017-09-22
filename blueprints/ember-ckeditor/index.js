9f6e52e146f859bc62bf5b75761a2f23407baa41
Cristina <cristina.dochitoiu1993@gmail.com>
2017-09-22 14:15:47 +0300

    remove bower dependencies

--- blueprints/ember-ckeditor/index.js
+++ blueprints/ember-ckeditor/index.js
@@ -1,7 +1,5 @@
 module.exports = {
   normalizeEntityName: function() {}, // no-op since we're just adding dependencies
 
-  afterInstall: function() {
-    return this.addBowerPackageToProject('ckeditor'); // is a promise
-  }
+  afterInstall: function() {}
 };
