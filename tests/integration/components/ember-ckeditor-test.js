0d4d9ec65128f86f94f51e9ad5552b41f1842ee5
Marcelo Andrade R <marcelo.andrade.r@gmail.com>
2018-12-04 10:06:29 -0500

    Upgrade to qunit tests

--- tests/integration/components/ember-ckeditor-test.js
+++ tests/integration/components/ember-ckeditor-test.js
@@ -1,21 +1,23 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('ember-ckeditor', 'Integration | Component | ember ckeditor', {
-  integration: true
-});
+module('Integration | Component | ember ckeditor', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders', function(assert) {
-  const done = assert.async();
-  this.render(hbs`{{ember-ckeditor value=text}}`);
+  test('it renders', async function(assert) {
+    const done = assert.async();
+    await render(hbs`{{ember-ckeditor value=text}}`);
 
-  const handle = setInterval(() => {
-    var text = this.$().text().trim();
-
-    if (text.indexOf('Rich Text Editor') >= 0) {
-      assert.ok(1, 'it rendered');
-      clearInterval(handle);
-      done();
-    }
-  }, 300);
+    const handle = setInterval(() => {
+      var text = this.element.textContent;
+      
+      if (text.indexOf('Rich Text Editor') >= 0) {
+        assert.ok(1, 'it rendered');
+        clearInterval(handle);
+        done();
+      }
+    }, 300);
+  });
 });
